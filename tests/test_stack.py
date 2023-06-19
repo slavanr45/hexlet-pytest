51c4c517119d57cf6c1def3892db62c85fb0d51a
slavanr45 <slava.r45@gmail.com>
2023-06-20 00:41:08 +0700

    add test_stack

---
+++ tests/test_stack.py
@@ -0,0 +1,36 @@
+def test_stack():
+    stack = []
+    # Добавляем два элемента в стек и затем извлекаем их
+    # Почему два? Так надежнее, чем один, а три — уже избыточно
+    stack.append('one')
+    stack.append('two')
+
+    assert stack.pop() == 'two'
+    assert stack.pop() == 'one'
+
+
+def test_stack1():
+    stack = []
+    stack.append('one')
+    stack.append('two')
+
+    assert stack.pop() == 'two'
+
+
+def test_stack2():
+    stack = []
+    stack.append('one')
+    stack.append('two')
+
+    stack.pop()
+    assert stack.pop() == 'one'
+
+
+def test_emptiness():
+    stack = []
+    assert not stack
+    stack.append('one')
+    assert bool(stack)  # not not stack
+
+    stack.pop()
+    assert not stack
